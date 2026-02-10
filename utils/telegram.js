40c2149983246adb378bcca3b4750337f37a5ce1
Firdavs <sharipovfirdavs5520@gmail.com>
2026-02-10 15:54:40 +0500

    lor muammo

--- utils/telegram.js
+++
@@ -1,9 +0,0 @@
-import sendTelegram from "./sendTelegram.js";
-
-export default async function telegram(text) {
-  try {
-    await sendTelegram(text);
-  } catch (e) {
-    console.error("TELEGRAM SEND ERROR:", e);
-  }
-}
